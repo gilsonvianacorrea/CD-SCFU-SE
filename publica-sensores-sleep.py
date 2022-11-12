689678c09b03b50af7f0143f3297b33e67568f83
Adenauer Yamin <adenauer@gmail.com>
2022-11-11 21:13:25 -0300

    Update and rename publica-sensores-sleep-2.py to publica-sensores-sleep.py

--- publica-sensores-sleep.py
+++ publica-sensores-sleep.py
@@ -11,7 +11,7 @@ def connect_scfu():
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         sta_if.active(True)
-        sta_if.connect('OsirMax_Wellington', '01091990')
+        sta_if.connect('SSID', 'Password')
         while not sta_if.isconnected():
             pass # wait till connection
     print('network config:', sta_if.ifconfig())
